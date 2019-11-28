5f81b30ca88d96e03d0e1c34c707bef72ab426a9
Daniel <gorsia@post.bgu.ac.il>
2019-11-28 21:40:46 +0200

    j

--- src/Customer.java
+++ src/Customer.java
@@ -1,6 +1,47 @@
 public class Customer {
 
 
-    public Customer() {
+    private String id;
+    private Address address;
+    private String phone;
+    private String email;
+
+    public Customer(String id, Address address, String phone, String email) {
+        this.id = id;
+        this.address = address;
+        this.phone = phone;
+        this.email = email;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 }
