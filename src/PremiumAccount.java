3044fbfc976add54a7c05308a13fccd445413f54
Daniel <gorsia@post.bgu.ac.il>
2019-12-08 21:57:57 +0200

    nnn

--- src/PremiumAccount.java
+++ src/PremiumAccount.java
@@ -1,8 +1,45 @@
+import java.util.ArrayList;
 import java.util.Date;
 
 public class PremiumAccount extends Account {
 
+    private ArrayList<Product> lProducts;
     public PremiumAccount(String id, String billingAddress, Date open, Date close, int balance) {
         super(id, billingAddress, open, close, balance);
+        lProducts = new ArrayList<>();
+    }
+
+    public ArrayList<Product> getlProducts() {
+        return lProducts;
+    }
+
+    public void setlProducts(ArrayList<Product> lProducts) {
+        this.lProducts = lProducts;
+    }
+
+    public boolean addProduct (Product product){
+        if(lProducts.contains(product) || product == null){
+            return false;
+        }
+        PremiumAccount _premiumAccout = product.getPremiumAccount();
+        if(_premiumAccout == null){
+            product.setPremiumAccount(this);
+            return false;
+        }
+        else if(!this.equals(_premiumAccout)){
+            _premiumAccout.removeProduct(product);
+            lProducts.add(product);
+            return true;
+        }
+        else{
+            lProducts.add(product);
+            return true;
+        }
+    }
+
+    private void removeProduct(Product product) {
+        if (lProducts.contains(product)) {
+            product.setPremiumAccount(null);
+        }
     }
 }
--- src/Product.java
+++ src/Product.java
@@ -5,13 +5,25 @@ public class Product {
     private String id;
     private String name;
     private Supplier supplier;
-    private ArrayList<LineItem> lineItems;
+    private ArrayList<LineItem> lLineItems;
+    private PremiumAccount premiumAccount;
 
+    //constructor without premiumAccount
     public Product(String id, String name, Supplier supplier) {
         this.id = id;
         this.name = name;
         this.supplier = supplier;
-        lineItems = new ArrayList<>();
+        lLineItems = new ArrayList<>();
+        premiumAccount = null;
+    }
+
+    //constructor with premiumAccount
+    public Product(String id, String name, Supplier supplier, PremiumAccount premiumAccount) {
+        this.id = id;
+        this.name = name;
+        this.supplier = supplier;
+        lLineItems = new ArrayList<>();
+        premiumAccount = premiumAccount;
     }
 
     public String getId() {
@@ -38,11 +50,11 @@ public class Product {
         if(lineItem == null){
             return false;
         }
-        if(lineItems.contains(lineItem)){
+        if(lLineItems.contains(lineItem)){
             return false;
         }
         else if(this.equals(lineItem.getProduct())){
-            lineItems.add(lineItem);
+            lLineItems.add(lineItem);
             return true;
         }
         else{
@@ -50,18 +62,39 @@ public class Product {
         }
     }
 
-    public boolean deleteLineItem(LineItem lineItem){
+    public boolean removeLineItem(LineItem lineItem){
         if(lineItem == null){
             return false;
         }
-        if(lineItems.contains(lineItem)){
-            lineItems.remove(lineItem);
+        if(lLineItems.contains(lineItem) && this.equals(lineItem.getProduct())){
+            lLineItems.remove(lineItem);
             return true;
         }
         else{
             return false;
         }
     }
+
+    public PremiumAccount getPremiumAccount() {
+        return premiumAccount;
+    }
+
+    public void setPremiumAccount(PremiumAccount premiumAccount) {
+        this.premiumAccount = premiumAccount;
+    }
+
+    public boolean deleteProduct(){
+        Supplier productSupplier = supplier;
+        this.supplier = null;
+        if(productSupplier != null){
+            productSupplier.deleteProduct(this);
+        }
+        for(LineItem lineItem : lLineItems){
+            lineItem.deleteLineItem();
+        }
+        return false;
+    }
+
 
 
 }
