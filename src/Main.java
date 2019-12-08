d82ecfeecfd715106eee5e38b6479d271730fcbf
Chen2908 <chengal@post.bgu.ac.il>
2019-12-08 18:04:23 +0200

    main

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,32 @@
 import java.sql.SQLOutput;
+import java.util.Date;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args){
-
-
+        Date openDate = new Date(2019, 12, 1);
+        Account daniAccount = new Account("Dani", "Israel", openDate, null, 0);
+        Account danaAccount = new PremiumAccount("Dana", "Israel", openDate, null, 0);
         Scanner sc = new Scanner(System.in);
         System.out.println("Login_id");
         String userName = sc.nextLine();
         System.out.println("Password:");
         String password = sc.nextLine();
+        switch (userName){
+            case "Dani":
+                while (! password.equals("Dani123")){
+                    System.out.println("Wrong password, please try again.");
+                    password = sc.nextLine();
+                }
+
+
+                break;
+
+            case "Dana":
+
+                break;
+        }
 
     }
 }
---
+++ src/PremiumAccount.java
@@ -0,0 +1,8 @@
+import java.util.Date;
+
+public class PremiumAccount extends Account {
+
+    public PremiumAccount(String id, String billingAddress, Date open, Date close, int balance) {
+        super(id, billingAddress, open, close, balance);
+    }
+}
