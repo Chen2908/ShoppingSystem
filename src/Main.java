1091b08a51205287fd4f02411caf4748abcff1b6
Daniel <gorsia@post.bgu.ac.il>
2019-12-08 16:43:44 +0200

    ba

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,16 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class Main {
 
+    public static void main(String[] args){
 
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Login_id");
+        String userName = sc.nextLine();
+        System.out.println("Password:");
+        String password = sc.nextLine();
+
+    }
 }
